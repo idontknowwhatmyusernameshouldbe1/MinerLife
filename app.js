4aea9a5e80640d0880c6a88c8603a4aaa436390f
idontknowwhatmyusernameshouldbe1 <minecraft2016roblox@gmail.com>
2026-02-03 17:03:24 +0100

    Ignore old app.js and use main.js

--- app.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-
-const out = document.getElementById("out");
-document.getElementById("clickme").addEventListener("click", () => {
-  out.textContent = "MinerLife is alive. Next step: add a 3D voxel demo!";
-});
